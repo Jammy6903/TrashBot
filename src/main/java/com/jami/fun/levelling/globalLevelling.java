7f99f2610b2ea8135172e73658764c149cb64d2f
Jammy6903 <benjamin6903@riseup.net>
2025-08-05 18:36:52 +0000

    owo

--- src/main/java/com/jami/fun/levelling/globalLevelling.java
+++ src/main/java/com/jami/fun/levelling/globalLevelling.java
@@ -1,18 +1,27 @@
 package com.jami.fun.levelling;
 
+import java.util.Random;
+
 import com.jami.database.user.user;
 
 public class globalLevelling {
   private static int expIncrement = 3;
+  private static int expVariation = 1;
   private static long expCooldown = 60;
 
   public static void incrementExp(long userId) {
     user u = new user(userId);
 
+    if (!u.getSettings().getEnabledFeatures().contains("levelling")) {
+      return;
+    }
+
     if (System.currentTimeMillis() - u.getLastMessage() <= expCooldown * 1000) {
       return;
     }
 
+    expIncrement = new Random().nextInt((expIncrement + expVariation) - (expIncrement - expVariation) + 1)
+        + (expIncrement - expVariation);
     u.setExp(u.getExp() + expIncrement);
     u.setLastMessage(System.currentTimeMillis());
 
--- src/main/java/com/jami/fun/levelling/guildLevelling.java
+++ src/main/java/com/jami/fun/levelling/guildLevelling.java
@@ -1,23 +1,34 @@
 package com.jami.fun.levelling;
 
+import java.util.Random;
+
 import com.jami.database.guild.guild;
 import com.jami.database.guild.guildUser;
+import com.jami.database.user.user;
 import com.jami.database.guild.guildSettings;
 
 public class guildLevelling {
   public static void incrementExp(long guildId, long userId) {
     guild g = new guild(guildId);
-    guildSettings settings = g.getSettings();
-    guildUser user = g.getUser(userId);
+    guildSettings s = g.getSettings();
+    guildUser gu = g.getUser(userId);
+    user u = new user(userId);
+
+    // Check if user has levelling feature enabled
+    if (!u.getSettings().getEnabledFeatures().contains("levelling")) {
+      return;
+    }
 
     // Check if cooldown time has elapsed since users last message
-    // if (System.currentTimeMillis() - user.getUserLastMessage() <=
-    // settings.getExpCooldown() * 1000) {
-    // return;
-    // }
+    if (System.currentTimeMillis() - gu.getUserLastMessage() <= s.getExpCooldown() * 1000) {
+      return;
+    }
 
-    user.setExp(user.getExp() + settings.getExpIncrement());
-    user.setUserLastMessage(System.currentTimeMillis());
+    long expIncrement = new Random()
+        .nextLong((s.getExpIncrement() + s.getExpVariation()) - (s.getExpIncrement() - s.getExpVariation()) + 1)
+        + (s.getExpIncrement() - s.getExpVariation());
+    gu.setExp(gu.getExp() + expIncrement);
+    gu.setUserLastMessage(System.currentTimeMillis());
 
     g.commit();
   }
